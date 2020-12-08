732f74e4401b42c6edbc011b8fdd7db83a5387bd
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-08 21:08:07 +0330

    Update and rename README.md to c.py

---
+++ c.py
@@ -0,0 +1,152 @@
+# c
+
+# color phint
+import os
+import sys
+import time
+from colorama import Fore
+
+def __color__():
+    os.system("clear")
+    try:
+        time.sleep(2)
+        print(Fore.GREEN + "Helloe . Welcome ;)")
+        time.sleep(2)
+        print(Fore.RED + "\n[1] - Color : RED")
+        time.sleep(0.3)
+        print(Fore.GREEN + "\n[2] - Color : GREEN")
+        time.sleep(0.3)
+        print(Fore.BLUE + "\n[3] - Color : BLUE")
+        time.sleep(0.3)
+        print(Fore.YELLOW + "\n[4] - Color : YELLOW")
+        time.sleep(0.3)
+        color = int(input(Fore.GREEN + "\n[!] - Enter Your Number 1 ~ 4 ==> "))
+        if color == "" or None :
+            try:
+                time.sleep(1)
+                print(Fore.RED + "|n[*] - Error : Your Number Is Not Found ;(")
+                time.sleep(1)
+                sys.exit()
+            except:
+                pass
+        if clear >= 5:
+            try:
+                time.sleep(1)
+                print(Fore.RED + "\n[*] - Error : Your Number Is Not Found ;(")
+                time.sleep(1)
+                sys.exit()
+            except:
+                pass
+        matn = input("\n[+] - Enter Your Text ==>  ")
+
+        if color == 1:
+            try:
+                time.sleep(1)
+                print("\nOk Pleass 5 sec Latter ;)")
+                time.sleep(1)
+                print(Fore.BLUE + "\n[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 5 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 4 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 3 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 2 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 1 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.RED + matn)
+            except:
+                try:
+                    time.sleep(2)
+                    print(Fore.BLUE + "\nThanck You ;)")
+                    sys.exit()
+                except:
+                    pass
+#________________           ( 2 )             ______________
+
+        if color == 2:
+            try:
+                time.sleep(1)
+                print("\nOk Pleass 5 sec Latter ;)")
+                time.sleep(1)
+                print(Fore.BLUE + "\n[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 5 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 4 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 3 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 2 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 1 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.GREEN + matn)
+            except:
+                try:
+                    time.sleep(2)
+                    print(Fore.YELLOW + "\nThanck You ;)")
+                    sys.exit()
+                except:
+                    pass
+# ________________       ( 3 )     ________________________________
+
+        if color == 3:
+            try:
+                time.sleep(1)
+                print("\nOk Pleass 5 sec Latter ;)")
+                time.sleep(1)
+                print(Fore.BLUE + "\n[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 5 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 4 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 3 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 2 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 1 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + matn)
+            except:
+                try:
+                    time.sleep(2)
+                    print(Fore.RED + "\nThanck You ;)")
+                    sys.exit()
+                except:
+                    pass
+
+# _________________________            (  4  )   ___________________________________
+
+        if color == 4:
+            try:
+                time.sleep(1)
+                print("\nOk Pleass 5 sec Latter ;)")
+                time.sleep(1)
+                print(Fore.BLUE + "\n[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 5 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 4 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 3 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 2 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.BLUE + "[" + Fore.YELLOW + "*" + Fore.BLUE + "]" + Fore.GREEN + " ~ " + Fore.GREEN + "Pleass 1 Sec Latter ...")
+                time.sleep(1)
+                print(Fore.YELLOW + matn)
+            except:
+                try:
+                    time.sleep(2)
+                    print(Fore.GREEN + "\nThanck You ;)")
+                    sys.exit()
+                except:
+                    pass
+        # __________
+        else:
+            try:
+                time.sleep(2)
+                print(Fore.RED + "\n[!] ~ Error : Your Number Or Number Text Is Not Found ;(")
+                time.sleep(2)
+                sys.exit()
+            except:
+                pass
+    except:
+        pass
+__color__()
